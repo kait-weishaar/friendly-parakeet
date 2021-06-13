4c24a3d50ecd24265217491026804c3fc1dc7d58
Kait <kweishaar@hawk.iit.edu>
2021-06-13 08:49:53 -0500

    deleted junk code

--- Develop/script.js
+++ Develop/script.js
@@ -1,55 +1,12 @@
 // Assignment code here
 
-//let arrayFromLowToHigh = (low, high) => {
-  //const array = [];
-  //for (let i = low; i <= high; i++) {
-  //  array.push(i);
-  //}
- // return array;
-//};
-
-
- //Generating Character Codes
-//const UPPERCASE_CODES = arrayFromLowToHigh(65, 90);
-//const LOWERCASE_CODES = arrayFromLowToHigh(97, 122);
-//const NUMBER_CODES = arrayFromLowToHigh(48, 57);
-//const SYMBOL_CODES = arrayFromLowToHigh(33, 47)
- // .concat(arrayFromLowToHigh(58, 64))
- // .concat(arrayFromLowToHigh(91, 96))
-  //.concat(arrayFromLowToHigh(123, 126));
-
-
-  
-//function secureMathRandom() {
-//	return window.crypto.getRandomValues(new Uint32Array(1))[0] / (Math.pow(2, 32) - 1);
-//}
-//function getRandomLower() {
-//	return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-//}
-//console.log(getRandomLower());
-//function getRandomUpper() {
-//	return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-//}
-//console.log(getRandomUpper());
-//function getRandomNumber() {
-//	return String.fromCharCode(Math.floor(secureMathRandom() * 10) + 48);
-//}
-//console.log(getRandomNumber());
-//function getRandomSymbol() {
-//	const symbols = '~!@#$%^&*()_+{}":?><;.,';
-//	return symbols[Math.floor(Math.random() * symbols.length)];
-//}
-//console.log(getRandomSymbol());
-
-
-
-
+//First get desired length
 const getLength = function() {
   
   length = prompt("What is your desired password length? Please enter a number between 8 and 128.");
   
   
-  if (parseInt(length)> 8 && parseInt(length)< 128) {
+  if (parseInt(length)>= 8 && parseInt(length)<= 128) {
     window.alert("Your password length is " + length);
   return parseInt(length);
    } else {
@@ -61,6 +18,8 @@ const getLength = function() {
 }
 getLength();
 
+
+//Determine which characters to include
 const confirmUppercase = function() {
   uppercase = window.confirm("Would you like to include uppercase letters?");
 }
@@ -81,17 +40,7 @@ const UPPERCASE = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', '
 const LOWERCASE = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 const NUMBER = ['1','2', '3', '4', '5', '6', '7', '8', '9'];
 const SYMBOL = [' ', '!', '"', '#', '$',' %', '&', "'", '(',' )', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '\\', '^','_', '`', '{', '|', '}', '~']
-//  .concat(arrayFromLowToHigh(58, 64))
-//  .concat(arrayFromLowToHigh(91, 96))
-//  .concat(arrayFromLowToHigh(123, 126));
 
-
-//const characters = [
- // ... uppercase ? [UPPERCASE] : [],
- // ... lowercase ? [LOWERCASE] : [],
- // ... numbers ? [NUMBER] : [],
- // ... symbols ? [SYMBOL] : [],
-// ];
 
 
 //Construct character set based on responses
@@ -108,7 +57,9 @@ const characters = unCharacters.flat(Infinity)
 console.log(characters);
 console.log(length);
 
-while (characters) {
+
+//Make sure at least one character set is selected
+while (characters===null) {
   window.alert("You need to select at least one character set to include in the password!")
 
   confirmUppercase();
